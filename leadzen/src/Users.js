048f895011b3a6c62e60e715dfa6e05b507455ac
Jpduker <jayaprakashnis619@gmail.com>
2023-02-02 23:33:23 +0530

    modified style of cards

--- leadzen/src/Users.js
+++ leadzen/src/Users.js
@@ -2,21 +2,19 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import {
 	Card,
-	CardHeader,
 	CardBody,
-	CardFooter,
 	Heading,
 	Text,
 	Button,
-	Flex,
-	SimpleGrid,
-	Stack,
-	List,
+	Box,
+	useDisclosure,
 } from "@chakra-ui/react";
-import "./Users.css"
+import "./Users.css";
 
 const Users = () => {
 	const [users, setUsers] = useState([]);
+
+	const { isOpen, onToggle } = useDisclosure();
 
 	const fetchData = () => {
 		axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
@@ -30,34 +28,53 @@ const Users = () => {
 
 	return (
 		<div className="body-div">
-    
 			{users.length > 0 &&
 				users.map((user) => (
+					<Card
+						className="card"
+						direction={{ base: "column", sm: "row" }}
+						variant="elevated"
+					>
+						<CardBody>
+							<Text>{user.company.name}</Text>
+						</CardBody>
 
-						<Card className="card" direction={{ base: "column", sm: "row" }} variant="elevated" >
-							<CardBody>
-								<Text>{user.company.name}</Text>
-							</CardBody>
-
-							<CardBody>
-								<Heading size="sm"> contact </Heading>
-								<Text>{user.phone}</Text>
-							</CardBody>
-							<CardBody>
-								<Heading size="sm"> city </Heading>
-								<Text>{user.address.city}</Text>
-							</CardBody>
-							<CardBody>
-								<Heading size="sm"> state </Heading>
-								<Text>{user.address.street}</Text>
-							</CardBody>
-							<Button variant="solid" colorScheme="blue" alignSelf="center" className="btn-details">
-								view details
-							</Button>
-						</Card>
-					
+						<CardBody>
+							<Heading size="sm"> contact </Heading>
+							<Text>{user.phone}</Text>
+						</CardBody>
+						<CardBody>
+							<Heading size="sm"> city </Heading>
+							<Text>{user.address.city}</Text>
+						</CardBody>
+						<CardBody>
+							<Heading size="sm"> state </Heading>
+							<Text>{user.address.street}</Text>
+						</CardBody>
+						<Button
+							variant="solid"
+							colorScheme="red"
+							alignSelf="center"
+							style={{
+								backgroundColor: "red",
+								borderRadius: "20px",
+								marginRight: "7px",
+							}}
+							className="btn"
+						>
+							view details
+						</Button>
+						<Box
+							className="additional-details"
+							style={{ display: isOpen ? "block" : "none" }}
+						>
+							<Heading size="sm">email</Heading>
+							<Text>{user.email}</Text>
+							<Heading size="sm">website</Heading>
+							<Text>{user.website}</Text>
+						</Box>
+					</Card>
 				))}
-            
 		</div>
 	);
 };
